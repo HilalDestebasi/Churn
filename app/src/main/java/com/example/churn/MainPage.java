21d491e58e120008dd71c97f379c74b241cfe9fd
zeynep hilal destebaşı <zeynep.destebasi@ceng.deu.edu.tr>
2022-05-07 19:41:44 +0300

    Churn

--- app/src/main/java/com/example/churn/MainPage.java
+++ app/src/main/java/com/example/churn/MainPage.java
@@ -1,21 +1,41 @@
 package com.example.churn;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 
 public class MainPage extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,31 +45,55 @@ public class MainPage extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main_page);
-        bottomNavigationView=findViewById(R.id.bottomNav);
+        bottomNavigationView = findViewById(R.id.bottomNav);
         bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container,new ItemFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new ItemFragment()).commit();
 
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod= new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+    String path = "";
+    ActivityResultLauncher<Intent> sActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Uri uri = data.getData();
+
+                    }
+                }
+            }
+    );
+
+    public void insertFileClick(View view) {
+        Intent data = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        data.setType("*/*");
+        data = Intent.createChooser(data, "Choose a file");
+        sActivityResultLauncher.launch(data);
+
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-            Fragment fragment=null;
-            switch (item.getItemId())
-            {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
 
                 case R.id.results:
-                    fragment=new Results();
+                    fragment = new Results();
                     break;
                 case R.id.products:
-                    fragment=new Products();
+                    fragment = new Products();
                     break;
                 case R.id.customers:
-                    fragment=new ItemFragment();
+                    fragment = new ItemFragment();
                     break;
             }
-            getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
             return false;
         }
     };
