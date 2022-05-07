0a09722dac8089e26dd7aafdbfead53f8a86058e
Cemal Üyüklü <cemapeldev@gmail.com>
2022-05-07 20:40:49 +0300

    absolute path

--- app/src/main/java/com/example/churn/MainPage.java
+++ app/src/main/java/com/example/churn/MainPage.java
@@ -25,11 +25,20 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Scanner;
 
 public class MainPage extends AppCompatActivity {
@@ -61,11 +70,95 @@ public class MainPage extends AppCompatActivity {
                     if (result.getResultCode() == Activity.RESULT_OK) {
                         Intent data = result.getData();
                         Uri uri = data.getData();
+                        System.out.println(uri.getPath());
+                        System.out.println(uri.getEncodedPath());
+                        System.out.println(uri.getLastPathSegment());
+                        File myFile = new File(uri.toString());
+                        System.out.println(myFile.getAbsolutePath());
+                        Context context = getApplicationContext();
+                        CharSequence text = myFile.getAbsolutePath();
+                        int duration = Toast.LENGTH_SHORT;
 
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                        try {
+                            upload(uri.toString().split(":")[1]);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
             }
     );
+    public void upload(String selectedPath) throws Exception {
+
+        HttpURLConnection connection = null;
+        DataOutputStream outputStream = null;
+        DataInputStream inputStream = null;
+
+        String pathToOurFile = selectedPath;
+        String urlServer = "http://3.126.50.162:3000/upload";
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+
+        FileInputStream fileInputStream = new FileInputStream(new File(
+                pathToOurFile));
+
+        URL url = new URL(urlServer);
+        connection = (HttpURLConnection) url.openConnection();
+
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+        connection.setUseCaches(false);
+
+        connection.setRequestMethod("POST");
+
+        connection.setRequestProperty("Connection", "Keep-Alive");
+        connection.setRequestProperty("Content-Type",
+                "multipart/form-data;boundary=" + boundary);
+
+        outputStream = new DataOutputStream(connection.getOutputStream());
+        outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+        outputStream
+                .writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\""
+                        + pathToOurFile + "\"" + lineEnd);
+        outputStream.writeBytes(lineEnd);
+
+        bytesAvailable = fileInputStream.available();
+        bufferSize = Math.min(bytesAvailable, maxBufferSize);
+        buffer = new byte[bufferSize];
+
+        bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+        while (bytesRead > 0) {
+            outputStream.write(buffer, 0, bufferSize);
+            bytesAvailable = fileInputStream.available();
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+        }
+
+        outputStream.writeBytes(lineEnd);
+        outputStream.writeBytes(twoHyphens + boundary + twoHyphens
+                + lineEnd);
+
+        int serverResponseCode = connection.getResponseCode();
+        String serverResponseMessage = connection.getResponseMessage();
+        Context context = getApplicationContext();
+        CharSequence text = serverResponseMessage;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        fileInputStream.close();
+        outputStream.flush();
+        outputStream.close();
+
+    }
 
     public void insertFileClick(View view) {
         Intent data = new Intent(Intent.ACTION_OPEN_DOCUMENT);
